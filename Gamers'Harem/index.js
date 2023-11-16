a8c74cf6a92d3c71701f9bb3b4eb25559c372ab0
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2023-11-16 23:55:15 +0100

    Add files via upload

--- Gamers'Harem/index.js
+++ Gamers'Harem/index.js
@@ -9,3 +9,10 @@ function search() {
     // Aggiungi qui la tua logica di ricerca con il termine di ricerca (searchTerm)
     // Esempio: alert("Esegui ricerca con termine: " + searchTerm);
 }
+
+function toggleMenu() {
+    var navbarLinks = document.querySelectorAll('.navbar-link');
+    navbarLinks.forEach(function(link) {
+        link.style.display = (link.style.display === 'none' || link.style.display === '') ? 'flex' : 'none';
+    });
+}
