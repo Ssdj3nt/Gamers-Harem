02b1a281bb9277d38739dbe54203fa3e089a9522
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2023-11-24 12:36:23 +0100

    Delete Gamers'Harem directory

--- Gamers'Harem/index.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-    var menuContent = document.querySelector(".menu-content");
-    menuContent.classList.add("menu-content-open");
-}
-
-function hideMenu() {
-    var menuContent = document.querySelector(".menu-content");
-    menuContent.classList.remove("menu-content-open");
-}
