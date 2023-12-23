3f8d89bb5bcc5b0fb002d3755005b166b6bf91c1
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2023-12-23 15:15:00 +0100

    Add files via upload

--- Gamers-Harem/app.js
+++ Gamers-Harem/app.js
@@ -3,25 +3,23 @@ function toggleMenu(menuId) {
     var loginBox = document.querySelector('.login-box');
     var navbar = document.querySelector('.navbar');
     var container = document.querySelector('.container');
-    var bottom = document.querySelector('.bottom-area');
+    var bottomarea = document.querySelector('.bottom-area');
 
     if (menuId === 'mobile-menu') {
         loginBox.classList.remove('show');
         nav.classList.toggle('show');
-
         container.style.display = nav.classList.contains('show') ? 'block' : 'none';
-        bottom.style.display = nav.classList.contains('show') ? 'block' : 'none';
+        bottomarea.style.display = nav.classList.contains('show') ? 'block' : 'none';
     } else if (menuId === 'login-menu') {
         nav.classList.remove('show');
         loginBox.classList.toggle('show');
-
         container.style.display = loginBox.classList.contains('show') ? 'none' : 'block';
-        bottom.style.display = loginBox.classList.contains('show') ? 'none' : 'block';
+        bottomarea.style.display = nav.classList.contains('show') ? 'block' : 'none';
     }
 
     if (!nav.classList.contains('show') && !loginBox.classList.contains('show')) {
         container.style.display = 'block';
-        bottom.style.display = 'block';
+        bottomarea.style.display = 'block';
     }
 
     if (nav.classList.contains('show') || loginBox.classList.contains('show')) {
@@ -34,7 +32,6 @@ function toggleMenu(menuId) {
 document.getElementById('mobile-menu').addEventListener('click', function () {
     toggleMenu('mobile-menu');
 });
-
 document.getElementById('login-menu').addEventListener('click', function () {
     toggleMenu('login-menu');
 });
@@ -49,7 +46,6 @@ function openModal() {
     body.classList.add('grayed-out');
     body.style.overflow = 'hidden';
 }
-
 function closeModal() {
     var overlay = document.getElementById('overlay');
     var modal = document.getElementById('myModal');
@@ -59,4 +55,41 @@ function closeModal() {
     modal.style.display = 'none';
     body.classList.remove('grayed-out');
     body.style.overflow = 'auto';
+}
+function openRegisterModal() {
+    closeModal();
+    var overlayRegister = document.getElementById('overlayRegister');
+    var registerModal = document.getElementById('registerModal');
+    var body = document.body;
+
+    document.getElementById('registerEmail').value = '';
+    document.getElementById('registerNickname').value = '';
+    document.getElementById('registerPassword').value = '';
+
+    overlayRegister.style.display = 'block';
+    registerModal.style.display = 'block';
+    body.classList.add('grayed-out');
+    body.style.overflow = 'hidden';
+}
+function closeRegisterModal() {
+    var overlayRegister = document.getElementById('overlayRegister');
+    var registerModal = document.getElementById('registerModal');
+    var body = document.body;
+
+    overlayRegister.style.display = 'none';
+    registerModal.style.display = 'none';
+    body.classList.remove('grayed-out');
+    body.style.overflow = 'auto';
+}
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('registerPassword');
+    var passwordToggle = document.querySelector('.password-toggle');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordToggle.innerHTML = '<img src="eye-closed.png" alt="Hide Password">';
+    } else {
+        passwordInput.type = 'password';
+        passwordToggle.innerHTML = '<img src="eye.png" alt="Show Password">';
+    }
 }
